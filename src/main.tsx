9edf0e3a46cfddb6b6da566b4a58cf6e298b0f09
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-10-29 18:40:15 -0300

    Add mui

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import App from './App.tsx'
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
